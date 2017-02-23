48605fffb50bd442df027f36591b914087a1bdc7
mehvishhashmi <mh449@njit.edu>
2017-02-22 20:16:38 -0500

    modifying files

--- add_product.php
+++ add_product.php
@@ -8,28 +8,25 @@ $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
 // Validate inputs
 if ($category_id == null || $category_id == false ||
         $code == null || $name == null || $price == null || $price == false) {
-	    $error = "Invalid product data. Check all fields and try again.";
-	        include('error.php');
-		} else {
-		    require_once('database.php');
+    $error = "Invalid product data. Check all fields and try again.";
+    include('error.php');
+} else {
+    require_once('database.php');
 
-		        // Add the product to the database  
-			    $query = 'INSERT INTO products
-			                     (categoryID, productCode, productName, listPrice)
-					                   VALUES
-							                    (:category_id, :code, :name, :price)';
-									        $statement = $db->prepare($query);
-										    $statement->bindValue(':category_id',
-										    $category_id);
-										        $statement->bindValue(':code', $code);
-											    $statement->bindValue(':name', $name);
-											        $statement->bindValue(':price',
-												$price);
-												    $statement->execute();
-												        $statement->closeCursor();
+    // Add the product to the database  
+    $query = 'INSERT INTO products_guitar1
+                 (categoryID, productCode, productName, listPrice)
+              VALUES
+                 (:category_id, :code, :name, :price)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':code', $code);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':price', $price);
+    $statement->execute();
+    $statement->closeCursor();
 
-													    // Display the Product
-													    List page
-													        include('index.php');
-														}
-														?>
+    // Display the Product List page
+    include('index.php');
+}
+?>
--- error.php
+++ error.php
@@ -4,20 +4,20 @@
 <!-- the head section -->
 <head>
     <title>My Guitar Shop</title>
-        <link rel="stylesheet" type="text/css" href="main.css" />
-	</head>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
 
-	<!-- the body section -->
-	<body>
-	    <header><h1>My Guitar Shop</h1></header>
+<!-- the body section -->
+<body>
+    <header><h1>My Guitar Shop</h1></header>
 
-	        <main>
-		        <h2 class="top">Error</h2>
-			        <p><?php echo $error; ?></p>
-				    </main>
+    <main>
+        <h2 class="top">Error</h2>
+        <p><?php echo $error; ?></p>
+    </main>
 
-				        <footer>
-					        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-						    </footer>
-						    </body>
-						    </html>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    </footer>
+</body>
+</html>
